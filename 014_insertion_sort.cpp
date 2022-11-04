443810bd0caebd4c4298f16c07b17715eec0e275
omshakti maurya <66057512+omshakti3159@users.noreply.github.com>
2022-11-04 22:21:43 +0530

    Added a program for insertion sort

--- 014_insertion_sort.cpp
+++ 014_insertion_sort.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
 using namespace std;
 
-void selectionSort(int arr[], int n){
-    for(int i=0; i<n; i++){
-        int min_idx = i;
-        for(int j=i+1; j<n; j++){
-            if(arr[min_idx] > arr[j]){
-                min_idx = j;
-            }
+void insertionSort(int arr[], int n){
+    for(int i=1; i<n; i++){
+        int key = arr[i];
+        int j = i-1;
+        while(key < arr[j] && j>=0){
+            arr[j+1] = arr[j];
+            --j;
         }
-        swap(arr[min_idx],arr[i]);
+        arr[j+1] = key;
     }
 }
 
@@ -25,7 +25,7 @@ int main(){
     int size = sizeof(arr)/sizeof(int);
     cout<<"Array before sorting - ";
     printArray(arr,size);
-    selectionSort(arr,size);
+    insertionSort(arr,size);
     cout<<"Array after sorting - ";
     printArray(arr,size);
     return 0;
